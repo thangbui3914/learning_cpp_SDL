4ed098e3ef6eaa89259b32617d79f9bda5d2e40d
thangbui3914 <thangbui040603@gmail.com>
2022-10-08 18:20:14 +0700

    Creating Window

---
+++ Project4/game.h
@@ -0,0 +1,29 @@
+#pragma once
+
+#include <SDL.h>
+#include <iostream>
+
+class Game
+{
+public:
+	Game();
+
+	~Game();
+
+	void init(const char*, int xpos, int ypos, int width, int height, bool fullscreen);
+
+	void handleEvents();
+
+	void update();
+
+	void render();
+
+	void clean();
+
+	bool running();
+
+private:
+	bool isRunning;
+	SDL_Window* window;
+	SDL_Renderer* renderer;
+};
---
+++ Project4/main.cpp
@@ -0,0 +1,21 @@
+#include "game.h"
+
+Game* game = nullptr;
+
+int main(int argc, char* argv[])
+{
+	game = new Game();
+
+	game->init("Fuck", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 800, 600, false);
+
+	while (game->running())
+	{
+		game->handleEvents();
+		game->update();
+		game->render();
+	}
+
+	game->clean();
+
+	return 0;
+}
